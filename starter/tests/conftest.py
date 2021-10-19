02508e740cb7f989c6202c1cecaed73778c236dc
ABN <adrian_bn@hotmail.com>
2021-10-19 16:08:59 +0200

    refactor dvc.read

--- starter/tests/conftest.py
+++ starter/tests/conftest.py
@@ -5,11 +5,10 @@ import pytest
 
 @pytest.fixture(scope='session')
 def data():
-    data = dvc.api.read(
-        "/starter/data/census_cleaned.csv",
-        repo="https://github.com/gaceladri/nd0821-c3-starter-code",
-        mode="r"
-    )
+    with dvc.api.open(
+        "starter/data/census_cleaned.csv",
+            repo="https://github.com/gaceladri/nd0821-c3-starter-code") as fd:
+        data = pd.read_csv(fd)
 
     if data is None:
         pytest.fail("The data could not be downloaded from dvc")
