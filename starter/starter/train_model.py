30879b5fa21c1e4d853fe9b1a8a615953119c41f
ABN <adrian_bn@hotmail.com>
2021-10-19 15:58:45 +0200

    resolve paths porting the code to train_model...

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,7 +1,6 @@
 # Script to train machine learning model.
 import argparse
 import logging
-import sys
 
 import dvc.api
 import numpy as np
@@ -11,17 +10,65 @@ from sklearn import set_config
 from sklearn.compose import (TransformedTargetRegressor, make_column_selector,
                              make_column_transformer)
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, StandardScaler
-
-sys.path.insert(1, './ml')
-from ml.model import create_slice
 
 set_config(display='diagram')
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def create_slice(test, column_name, pipeline, encoder):
+    uniques = test[column_name].unique()
+
+    metrics_file = open(
+        'starter/model/%s_slice_output.txt' %
+        (column_name), "w")
+    metrics_file.write(
+        "Performance results for the slices of the feature %s" % (column_name))
+    metrics_file.write("\n")
+
+    for unique in uniques:
+        slice = test[test[column_name] == unique]
+        x_test, y_test = slice.drop(['salary'], axis=1), slice['salary']
+        preds = pipeline.predict(x_test)
+        precision, recall, fbeta = compute_model_metrics(
+            encoder.transform(y_test), preds)
+        metrics_file.write("Precision: %f" % (precision))
+        metrics_file.write("\n")
+        metrics_file.write("Recall: %f" % (recall))
+        metrics_file.write("\n")
+        metrics_file.write("F1: %f" % (fbeta))
+        metrics_file.write("\n")
+        metrics_file.write("*-*" * 5)
+        metrics_file.write("\n")
+
+    metrics_file.close()
 
 
 def create_preprocessor():
