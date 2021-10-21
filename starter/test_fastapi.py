ea756393726bddf6fae7dd67ce33f16056bd31a1
ABN <adrian_bn@hotmail.com>
2021-10-21 16:39:38 +0200

    update tests

--- starter/test_fastapi.py
+++ starter/test_fastapi.py
@@ -84,20 +84,20 @@ def test_inference_above():
 
 
 def test_inference_below():
-    request_data = {'age': 52,
-                    'workclass': 'Self-emp-not-inc',
-                    'fnlgt': 209642,
-                    'education': 'HS-grad',
-                    'education-num': 9,
+    request_data = {'age': 42,
+                    'workclass': 'Private',
+                    'fnlgt': 159449,
+                    'education': 'Bachelors',
+                    'education-num': 13,
                     'marital-status': 'Married-civ-spouse',
                     'occupation': 'Exec-managerial',
                     'relationship': 'Husband',
                     'race': 'White',
                     'sex': 'Male',
-                    'capital-gain': 0,
+                    'capital-gain': 5178,
                     'capital-loss': 0,
-                    'hours-per-week': 45,
-                    'native-country': 'United-States', }
+                    'hours-per-week': 40,
+                    'native-country': 'United-States'}
 
     r = client.post(
         "http://127.0.0.1:8000/prediction/",
